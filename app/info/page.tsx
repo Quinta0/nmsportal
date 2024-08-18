e8faba7416095a979e95213250e89af817af3ae3
Quinta <0pietroquintavalle0@gmail.com>
2024-08-18 18:07:40 +0200

    For Rask

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -53,6 +53,12 @@ export default function InfoPage() {
                     <h2 className="text-2xl font-semibold mb-4">How Portal Addresses Work</h2>
                     <p>In No Man's Sky, portal addresses consist of 12 glyphs chosen from a set of 16 possible glyphs. The first glyph represents the planetary index, the next two the solar system index, followed by one for the planet index, and the remaining eight for specific X, Y, and Z coordinates within that system. By inputting a specific sequence of glyphs, players can teleport to corresponding locations across the vast game world.</p>
                 </section>
+                <Separator className="mb-8"/>
+                <section>
+                    <h2 className="text-2xl font-semibold mb-4">Special Thanks</h2>
+                    <p>Thanks to the The Games Hub and the No Man's Sky community for their support and inspiration.</p>
+                    <p>And to Rask Rex for helping me with lots of feedback!</p>
+                </section>
             </main>
         </div>
     );
