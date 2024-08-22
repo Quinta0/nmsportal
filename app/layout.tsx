d498f694c7a6e829f702725699ca092ca1156d50
Quinta <0pietroquintavalle0@gmail.com>
2024-08-22 02:43:57 +0200

    Layout to the rescue?

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "@/components/AuthProvider";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,13 +12,19 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+      <body className={inter.className}>
+      <AuthProvider>
+        <ErrorBoundary>
+          {children}
+        </ErrorBoundary>
+      </AuthProvider>
+      </body>
+      </html>
   );
 }
